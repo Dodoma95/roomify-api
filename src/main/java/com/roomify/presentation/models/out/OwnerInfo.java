f45d04cdc6f3c2cf3666ea61d383460dd90f1a94
Dodoma <dorian.maillard@maif.fr>
2026-06-19 09:36:00 +0200

    fix : add description + avatarUrl  graphql bookings

--- src/main/java/com/roomify/presentation/models/out/OwnerInfo.java
+++ src/main/java/com/roomify/presentation/models/out/OwnerInfo.java
@@ -1,10 +1,13 @@
 package com.roomify.presentation.models.out;
 
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 public record OwnerInfo(
         @NonNull Long id,
         @NonNull String firstName,
         @NonNull String lastName,
-        @NonNull String email
+        @NonNull String email,
+        @Nullable String description,
+        @Nullable String avatarUrl
 ) {}
